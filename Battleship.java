f46d605078476af1fe9ccdb22d5426794dd40c14
Jason Xie <jxie40@stuy.edu>
2023-03-11 19:43:36 -0500

    Creating/Placing ships, Checks for misses (but not hits). Bit scuff, needs to be reviewed/worked on later. Needs still: Check for hit, winning

--- Battleship.java
+++ Battleship.java
@@ -1,15 +1,76 @@
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Battleship {
   private static int length;
   //coordinates of front of ship
-  private static int[] front;
+  private static int frontRow;
+  private static int frontCol;
   //coordinates of back of ship
-  private static int[] back;
+  private static int backRow;
+  private static int backCol;
 
-  public Battleship(int length, int frontX, int frontY, int backX, int backY) {
-    length = length;
-    front = new int[] {frontX, frontY};
-    back = new int[] {backX, backY};
+  public Battleship(String coordsFront, String coordsBack) { //ASSUME NO DIAGONALS
+    frontRow = Integer.parseInt(coordsFront.substring(1, coordsFront.length()));
+    frontCol = coordsFront.charAt(0) - 64;
+    backRow = Integer.parseInt(coordsBack.substring(1, coordsBack.length()));
+    backCol = coordsBack.charAt(0) - 64;
+    length = Math.abs(frontRow - backRow) + Math.abs(frontCol - backCol);
+  }
+
+  public int getFrontX() {
+    return frontCol;
+  }
+  public int getFrontY() {
+    return frontRow;
+  }
+  public int getBackX() {
+    return backCol;
+  }
+  public int getBackY() {
+    return backRow;
+  }
+
+  public void place(int[][] board, int marker) { //boolean?
+    //System.out.println("Front: " + this.getFrontX() + ", " + this.getFrontY());
+    //System.out.println("Back: " + this.getBackX() + ", " + this.getBackY());
+    if (getFrontX() - getBackX() == 0) {
+      for (int row = 0; row <= Math.abs(getFrontY() - getBackY()); row ++) {
+        board[getFrontY() + row][getFrontX()] = marker;
+      }
+    }
+    if (getFrontY() - getBackY() == 0) {
+      for (int col = 0; col <= Math.abs(getFrontX() - getBackX()); col ++) {
+        board[getFrontY()][getFrontX() + col] = marker;
+      }
+    }
   }
 
 
+
+  //FOR TESTING
+  public static void main(String[] args) {
+    Battleship testShip = new Battleship("A1", "A2");
+    ArrayList<Battleship> testList = new ArrayList<Battleship>();
+    int[][] testBoard = new int[5][5];
+    for (int i = 0; i < 5; i ++) {
+      for (int j = 0; j < 5; j ++) {
+        testBoard[i][j] = 0;
+      }
+    }
+    for (int i = 0; i < 5; i ++) {
+      System.out.println(Arrays.toString(testBoard[i]));
+    }
+    testList.add(testShip);
+
+    //System.out.println("Front: " + testShip.getFrontX() + ", " + testShip.getFrontY());
+    //System.out.println("Back: " + testShip.getBackX() + ", " + testShip.getBackY());
+
+    for (int i = 0; i < testList.size(); i ++) {
+      testList.get(i).place(testBoard, (i + 1) * -1);
+    }
+
+    for (int i = 0; i < 5; i ++) {
+      System.out.println(Arrays.toString(testBoard[i]));
+    }
+  }
 }
--- Game.java
+++ Game.java
@@ -6,43 +6,91 @@ public class Game {
   private static ArrayList<Battleship> playerOneShips;
   private static int[][] playerTwoBoard;
   private static ArrayList<Battleship> playerTwoShips;
-//BOARDS: -1 = battleship, 0 = empty, 1 = cleared
+//BOARDS: 2 = miss, 0 = empty, 1 = hit
 
 
   public static void main(String[] args) {
     int turn = 0;
     int rows = 0;
     int columns = 0;
+    int numberShips = 0;
     String input = "";
     Scanner in = new Scanner(System.in);
 
     System.out.println("How many rows?");
     while(rows <= 0){
-      //move cursor
-      rows = Integer.parseInt(in.nextLine());
+      rows = in.nextInt();
     }
     System.out.println("How many columns?");
     while(columns <= 0){
-      //move cursor
-      columns = Integer.parseInt(in.nextLine());
+      columns = in.nextInt();;
+    }
+    System.out.println("How many ships per player?");
+    while(numberShips <= 0){
+      numberShips = in.nextInt();;
     }
 
     System.out.println("Rows: " + rows);
     System.out.println("Columns (at most 26): " + columns);
+    System.out.println("Ships: " + numberShips);
 
     playerOneBoard = createBoard(rows, columns);
     playerTwoBoard = createBoard(rows, columns);
+    playerOneShips = new ArrayList<Battleship>(numberShips);
+    playerTwoShips = new ArrayList<Battleship>(numberShips);
 
+    //PLACING SHIPS
+    clearTerminal();
+    gotoTop();
+
+    System.out.println("PLAYER ONE: CREATE YOUR SHIPS");
+    printBoard(playerOneBoard, false);
+    for (int i = 0; i < numberShips; i ++) { //FOR PLAYER ONE
+      String front = "";
+      String back = "";
+      System.out.println("Front of Ship " + (i + 1) + " (A1 format): ");
+      front = in.next();
+
+      System.out.println("Back of Ship " + (i + 1) + " (A1 format): ");
+      back = in.next();
+      Battleship ship = new Battleship(front, back);
+      playerOneShips.add(ship);
+      ship.place(playerOneBoard, (i + 1) * -1);
+      printBoard(playerOneBoard, false);
+    }
+    //NOTE: NO DELAY FOR LAST SHIP
+    
+    clearTerminal();
+    gotoTop();
+
+    System.out.println("PLAYER TWO: CREATE YOUR SHIPS");
+    printBoard(playerTwoBoard, false);
+    for (int i = 0; i < numberShips; i ++) { //FOR PLAYER ONE
+      String front = "";
+      String back = "";
+      System.out.println("Front of Ship " + (i + 1) + " (A1 format): ");
+      front = in.next();
+
+      System.out.println("Back of Ship " + (i + 1) + " (A1 format): ");
+      back = in.next();
+      Battleship ship = new Battleship(front, back);
+      playerTwoShips.add(ship);
+      ship.place(playerTwoBoard, (i + 1) * -1);
+      printBoard(playerTwoBoard, false);
+    }
+    //NOTE: NO DELAY FOR LAST SHIP
+
+    //BOMBING PHASE (AFTER PLACING SHIPS)
     while(!(input.equals("stop"))){
       clearTerminal();
       gotoTop();
       turn ++; // odd turns = player 1, even turns = player 2
       if (turn % 2 == 0) {//player 2's turn
-        printBoard(playerOneBoard);
+        System.out.println("<PLAYER TWO'S TURN>");
+        printBoard(playerOneBoard, true);
         System.out.println("<PLAYER ONE'S BOARD>");
         System.out.println("PLEASE CHOOSE COORDINATES (EX: A1)");
-
-        input = in.nextLine();
+        input = in.next();
 
         if (attack(input, playerOneBoard)) {//RETURN HIT MESSAGHE
           //
@@ -51,14 +99,16 @@ public class Game {
           clearTerminal();
           gotoTop();
           System.out.println("You missed!");
-          printBoard(playerOneBoard);
+          printBoard(playerOneBoard, true);
+          in.nextLine();
         }
       }
       else {//player 1's turn
-        printBoard(playerTwoBoard);
+        System.out.println("<PLAYER ONE'S TURN>");
+        printBoard(playerTwoBoard, true);
         System.out.println("<PLAYER TWO'S BOARD>");
         System.out.println("PLEASE CHOOSE COORDINATES (EX: A1)");
-        input = in.nextLine();
+        input = in.next();
 
         if (attack(input, playerTwoBoard)) {//RETURN HIT MESSAGHE
           //
@@ -67,11 +117,12 @@ public class Game {
           clearTerminal();
           gotoTop();
           System.out.println("You missed!");
-          printBoard(playerTwoBoard);
+          printBoard(playerTwoBoard, true);
+          in.nextLine();
         }
       }
       System.out.println("Press Enter for Next Turn");
-      input = in.nextLine();
+      in.nextLine();
 
     }
   }
@@ -92,7 +143,7 @@ public class Game {
     }
     return board;
   }
-  public static void printBoard(int[][] board) {
+  public static void printBoard(int[][] board, boolean concealed) {
     for (int i = 0; i < board.length; i ++) {
       for (int j = 0; j < board[i].length; j ++) {
         if (board[i][j] >= 'A' && board[i][j] <= 'Z') {
@@ -102,7 +153,7 @@ public class Game {
           if (board[i][j] == 1 && j > 0) { //HIT
             System.out.print("X");
           }
-          else if (board[i][j] == -1) { //MISS
+          else if (board[i][j] == 2 && j > 0) { //MISS
             System.out.print("O");
           }
           else {
@@ -110,7 +161,12 @@ public class Game {
               System.out.print(board[i][j]);
             }
             else {
-              System.out.print(" ");
+              if (concealed || board[i][j] == 0) {
+                System.out.print(" ");
+              }
+              else {
+                System.out.print(board[i][j] * -1);
+              }
             }
           }
         }
@@ -124,11 +180,11 @@ public class Game {
 
   //GAME METHODS
   public static boolean attack(String input, int[][] beingAttacked) { //ASSUME IT IS IN A1 FORMAT
-    int row = Integer.parseInt(input.substring(1, 2));
+    int row = Integer.parseInt(input.substring(1, input.length()));
     int col = input.charAt(0) - 64;
 
     if (beingAttacked[row][col] == 0) {
-      beingAttacked[row][col] = -1;
+      beingAttacked[row][col] = 2;
       return false;
     }
     //CHECK IF COORDINATE IS BATTLESHIP COORDINATE
