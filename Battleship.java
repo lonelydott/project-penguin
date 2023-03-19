320b802e3a3218a9f525e91c42f862deefdf7be6
Jason X <jxie40@stuy.edu>
2023-03-18 21:43:15 -0400

    Fixed why different Arraylist referred to identical battleship, private variables were static.

--- Battleship.java
+++ Battleship.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 public class Battleship {
-  private static int length;
+  private int length;
   //coordinates of front of ship
-  private static int frontRow;
-  private static int frontCol;
+  private int frontRow;
+  private int frontCol;
   //coordinates of back of ship
-  private static int backRow;
-  private static int backCol;
+  private int backRow;
+  private int backCol;
 
   public Battleship(String coordsFront, String coordsBack) { //ASSUME NO DIAGONALS
     frontRow = Integer.parseInt(coordsFront.substring(1, coordsFront.length()));
     frontCol = coordsFront.charAt(0) - 64;
     backRow = Integer.parseInt(coordsBack.substring(1, coordsBack.length()));
     backCol = coordsBack.charAt(0) - 64;
-    length = Math.abs(frontRow - backRow) + Math.abs(frontCol - backCol);
+    length = Math.abs(frontRow - backRow) + Math.abs(frontCol - backCol) + 1;
   }
 
   public int getFrontX() {
@@ -28,6 +28,17 @@ public class Battleship {
   }
   public int getBackY() {
     return backRow;
+  }
+  public int getLength() {
+    return length;
+  }
+
+  public String toString() {
+    return "front: " + (char)(getFrontX() + 64) + getFrontY() + ", " + "back: " + (char)(getBackX() + 64) + getBackY();
+  }
+
+  public void minusOne() {
+    length --;
   }
 
   public void place(int[][] board, int marker) { //boolean?
@@ -49,8 +60,31 @@ public class Battleship {
 
   //FOR TESTING
   public static void main(String[] args) {
-    Battleship testShip = new Battleship("A1", "A2");
     ArrayList<Battleship> testList = new ArrayList<Battleship>();
+    for (int i = 1; i < 3; i ++) {
+      Battleship testShip = new Battleship("A" + i, "A" + (i + 1));
+      //System.out.println("A" + i + ", " + "A" + (i + 1));
+      testList.add(testShip);
+      System.out.println(testShip);
+    }
+    // Battleship testShip = new Battleship("A1", "A2");
+    // System.out.println(testShip.getFrontX());
+    // Battleship testShip1 = new Battleship("B2", "B3");
+    // System.out.println(testShip.getFrontX());
+    // System.out.println("A1".charAt(0) - 64);
+    //
+    //
+    // System.out.println(testShip);
+    // System.out.println(testShip1);
+    //
+    // ArrayList<Battleship> testList = new ArrayList<Battleship>();
+    // testList.add(testShip);
+    // System.out.println(testList);
+    // testList.add(testShip1);
+    System.out.println(testList);
+
+
+    /*
     int[][] testBoard = new int[5][5];
     for (int i = 0; i < 5; i ++) {
       for (int j = 0; j < 5; j ++) {
@@ -72,5 +106,6 @@ public class Battleship {
     for (int i = 0; i < 5; i ++) {
       System.out.println(Arrays.toString(testBoard[i]));
     }
+    */
   }
 }
